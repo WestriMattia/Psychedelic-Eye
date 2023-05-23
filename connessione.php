59c4993ab56e4119703ae390a4dc1ce98c38d4fd
Davide <98748215+DaviApo145@users.noreply.github.com>
2023-05-24 00:20:09 +0200

    db e miglioramenti

---
+++ connessione.php
@@ -0,0 +1,29 @@
+<<<<<<< HEAD
+<?php
+$servername = "localhost";
+$username = "root";
+$pass = "";
+$dbname = "pe";
+
+$mysql = new mysqli($servername,$username,$pass,$dbname);
+$conn = mysqli_connect($servername, $username, $pass, $dbname);
+
+if (!$conn) {
+  die("Connessione fallita: " . mysqli_connect_error());
+}
+?>
+=======
+<?php
+$servername = "localhost";
+$username = "root";
+$pass = "";
+$dbname = "pe";
+
+$mysql = new mysqli($servername,$username,$pass,$dbname);
+$conn = mysqli_connect($servername, $username, $pass, $dbname);
+
+if (!$conn) {
+  die("Connessione fallita: " . mysqli_connect_error());
+}
+?>
+>>>>>>> 0f85ff6 (modifica db e piccoli miglioramenti)
---
+++ disponibilita.php
@@ -0,0 +1,15 @@
+<<<<<<< HEAD
+<?php
+    require_once('connessione.php');
+    $idProdotto = $_SESSION['idProdotto'];
+    $query = "SELECT taglie.* FROM prodotto 
+    INNER JOIN taglie ON taglie.idTaglia=prodotto.idTaglia WHERE prodotto.idProdotto = $idProdotto";
+?>
+=======
+<?php
+    require_once('connessione.php');
+    $idProdotto = $_SESSION['idProdotto'];
+    $query = "SELECT taglie.* FROM prodotto 
+    INNER JOIN taglie ON taglie.idTaglia=prodotto.idTaglia WHERE prodotto.idProdotto = $idProdotto";
+?>
+>>>>>>> 0f85ff6 (modifica db e piccoli miglioramenti)
